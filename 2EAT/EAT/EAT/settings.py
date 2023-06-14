ac1775b34c468d8b3dd99eaa7bf50541e81b0cc0
Stephanie-siewe <85189972+Stephanie-siewe@users.noreply.github.com>
2023-06-14 15:46:15 +0100

    Update settings.py

--- 2EAT/EAT/EAT/settings.py
+++ 2EAT/EAT/EAT/settings.py
@@ -80,8 +80,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'EAT.urls'
 
-#CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOWED_ORIGINS = ['*']
 #CORS_ORIGIN_WHITELIST = (
  #   'https://backend-toeat-production.up.railway.app/',
   #  'http://localhost:8100',
